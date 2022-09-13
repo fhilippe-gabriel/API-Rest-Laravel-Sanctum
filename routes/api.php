3b23b988d8a7b64fab8c2acbaf135da4caa44dd9
Fhilippe Gabriel <fhilippegabriel15@gmail.com>
2022-09-13 16:43:30 -0300

    :up: atualizaction project

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
+
+Route::get('/products', 'ProductController@index');
+Route::post('/products', 'ProductController@store');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
